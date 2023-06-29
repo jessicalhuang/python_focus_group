d2dd7d01bfe08bb8e65d377ef0122646f726ec2e
jessicalhuang <jehuang@ucdavis.edu>
2023-06-29 13:37:29 -0700

    Create comments.py

---
+++ session_04/comments.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+print("Here is an example of an inline comment") # Inline comment
+
+# Print my name
+print("Jessica")
+
+'''
+
+Script Usage:
+To run comments.py, use the command "python3 comments.py" at the commandline.
+
+Output:
+The output of comments.py are miscellaneous statements used as arguments to demonstrate the print() function
+
+'''
+
+'''
+# Prints numbers 1-20
+for i in range(1,21):
+    print(i)
+'''
+
+# Prints the name of all of Viki's pets
+pet_names = ["Stella", "Blueverry", "Sunny", "Bird", "Zoscia", "New Pet"]
+for pet in pet_names:
+    print(pet)
