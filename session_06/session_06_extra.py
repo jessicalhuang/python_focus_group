89bbab3f383c0bbdb952d546492c6473131f86d8
jessicalhuang <jehuang@ucdavis.edu>
2023-06-29 15:04:27 -0700

    session_06 extra practice

---
+++ session_06/session_06_extra.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+exercise_1 = print('Anthony J. D\'Angelo said, "Develop a passion for learning. If you do, you will never cease to grow."')
+
+exercise_2 = 'The door swung open to reveal pink giraffes and red elephants'
+print(exercise_2.upper())
+
+exercise_3 = 'departure'
+print(exercise_3.find('a'))
+
+exercise_4 = 'The tattered work gloves speak of the many hours of hard labor he endured throughout his life'
+print(len(exercise_4))
+
+breed = "Great Dane"
+exercise_5 = print(f'The {breed} looked more like a horse than a dog.')
+
+breed = "Mastiff"
+exercise_6 = print(f'The {breed} looked more like a horse than a dog.')
